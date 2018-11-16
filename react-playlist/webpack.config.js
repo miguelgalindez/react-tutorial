7d66df8451b51e9514c4c9e75da576020232559b
Miguel Angel <miguelgalindez@unicauca.edu.co>
2018-11-15 21:34:37 -0500

    Initial commit

---
+++ react-playlist/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    devServer: {
+        inline: false,
+        contentBase: "src/",
+        hot: true,
+        port: 1234
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
